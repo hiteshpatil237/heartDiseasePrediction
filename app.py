e2d3c164d2253ba219b06c72a0d7bb7631d1d0af
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-07-10 23:08:05 -0400

    Added the final html code and connected functions to handle form

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+#Create an app object using the Flask class. 
+app = Flask(__name__)
+
+#Load the trained model. (Pickle file)
+model = pickle.load(open('model/model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()] 
+    features = [np.array(int_features)]  
+    prediction = model.predict(features)  
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run()
