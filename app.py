b76d70ac00e94ad850f6f46ca639dc3ebb1e7a4d
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-07-11 22:52:55 -0400

    Added small changes to wsgi.py

--- app.py
+++ app.py
@@ -3,22 +3,25 @@ from flask import Flask, request, render_template
 import pickle
 
 #Create an app object using the Flask class. 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-#Load the trained model. (Pickle file)
-model = pickle.load(open('model/model.pkl', 'rb'))
+    #Load the trained model. (Pickle file)
+    model = pickle.load(open('model/model.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    @app.route('/')
+    def home():
+        return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+    @app.route('/predict',methods=['POST'])
+    def predict():
 
-    int_features = [float(x) for x in request.form.values()] 
-    features = [np.array(int_features)]  
-    prediction = model.predict(features)  
+        int_features = [float(x) for x in request.form.values()] 
+        features = [np.array(int_features)]  
+        prediction = model.predict(features)  
 
-    output = round(prediction[0], 2)
+        output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
+        return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
+    
+    return app
